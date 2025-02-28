caa0f37185bfeaf7148a4a36c7092999a4c25adc
Jezarr <jezarrilli@hotmail.com>
2025-02-28 18:39:24 -0500

    subcategory buttons hide

--- Generate_Manifest.py
+++ Generate_Manifest.py
@@ -1,8 +1,11 @@
 import os
 import json
 
-items = []
+with open("manifest.json") as f:
+    items = json.load(f)
+    existing_images = {item["image"] for item in items}
 for f in os.listdir("images"):
-    items.append({"image": f"images/{f}", "category": "", "subCategory": ""})
+    if f"images/{f}" not in existing_images:
+        items.append({"image": f"images/{f}", "category": "", "subCategory": ""})
 with open("manifest.json", "w", encoding="utf-8") as f:
     json.dump(items, f, indent=2)
